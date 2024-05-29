21f9eefe2d902977da2ec75583baefcb34cf86f8
Ayomitide OAJ <ayomitide007@gmail.com>
2024-05-29 16:39:41 +0100

    app header and description

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import '@/styles/globals.css'
 import AuthGuard from '@/utils/auth-guard'
 import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
@@ -12,6 +13,15 @@ export default function App({ Component, pageProps }: AppProps) {
       refetchInterval={500}
       refetchOnWindowFocus
     >
+      <Head>
+        <title>Palremit Admin Dashboard</title>
+        <meta name="theme-color" content="#E6E7EB" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        />
+        <meta name="description" content="Palremit Admin Dashboard" />
+      </Head>
       {
         // @ts-ignore
         Component.auth ? (
