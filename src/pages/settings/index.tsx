42bb820b9c4711e7c2947e9c18640157c0310741
Ayomitide OAJ <ayomitide007@gmail.com>
2024-05-29 13:30:05 +0100

    hide admin

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -10,7 +10,7 @@ const Settings = () => {
   const [isOpen, setIsOpen] = useState(false)
   const tabList = [
     'Profile',
-    'Admin',
+    // 'Admin',
     // 'Password'
   ]
   const tabPanels = [
