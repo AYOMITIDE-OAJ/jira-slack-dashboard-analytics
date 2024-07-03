047257106d43b1397c69935992751ab51299d8e3
Ayomitide OAJ <ayomitide007@gmail.com>
2024-07-03 12:04:31 +0100

    chore: selfie upload preview

--- src/components/card-modal.tsx
+++ src/components/card-modal.tsx
@@ -26,18 +26,30 @@ export default function CardDetailsModal({
   handleFileChange,
 }: Props) {
   const [fileName, setFileName] = useState('')
+  const [preview, setPreview] = useState(null)
 
   const onFileChange = (event: any) => {
     const file = event.target.files[0]
     if (file) {
       setFileName(file.name)
       handleFileChange(event)
+
+      const reader = new FileReader()
+      reader.onloadend = () => {
+        setPreview((reader as any).result)
+      }
+      reader.readAsDataURL(file)
     }
   }
 
   const closeModalAction = () => {
     setIsOpen(false)
     setFileName('')
+    setPreview(null)
+  }
+
+  const triggerFileInput = () => {
+    ;(document as any).getElementById('dropzone-file').click()
   }
 
   return (
@@ -97,41 +109,50 @@ export default function CardDetailsModal({
             Retry Request
           </Button>
           <div className="mt-8 gap-4">
-            {/* <input className='p-2 border-none' type="file" accept="image/*" onChange={handleFileChange} /> */}
-
-            <div className="flex w-full items-center justify-center">
-              <label className="flex h-40 w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 bg-gray-50 hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:border-gray-500  dark:hover:bg-gray-800">
-                <div className="flex flex-col items-center justify-center pb-6 pt-5">
-                  <svg
-                    className="mb-4 h-8 w-8 text-gray-500 dark:text-gray-400"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 20 16"
-                  >
-                    <path
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-                    />
-                  </svg>
-                  <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                    <span className="font-semibold">Click to upload</span> or
-                    drag and drop
-                  </p>
-                  <p className="text-xs text-gray-500 dark:text-gray-400">
-                    SVG, PNG, JPG or GIF (MAX. 800x400px)
-                  </p>
-                </div>
-                <input
-                  onChange={onFileChange}
-                  id="dropzone-file"
-                  type="file"
-                  className="hidden"
+            <div className="flex flex-col items-center justify-center pb-6 pt-5">
+              <input
+                onChange={onFileChange}
+                id="dropzone-file"
+                type="file"
+                className="hidden"
+              />
+              {preview ? (
+                <Image
+                  src={preview}
+                  alt="Uploaded Preview"
+                  className="mx-auto my-3 block rounded-xl object-contain"
+                  height={150}
+                  width={150}
+                  onClick={triggerFileInput}
                 />
-              </label>
+              ) : (
+                <label htmlFor="dropzone-file" className="cursor-pointer">
+                  <div className="flex flex-col items-center justify-center border-2 border-dashed border-gray-300 p-6">
+                    <svg
+                      className="mb-4 h-8 w-8 text-gray-500 dark:text-gray-400"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 20 16"
+                    >
+                      <path
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                      />
+                    </svg>
+                    <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                      <span className="font-semibold">Click to upload</span> or
+                      drag and drop
+                    </p>
+                    <p className="text-xs text-gray-500 dark:text-gray-400">
+                      SVG, PNG, JPG or GIF (MAX. 800x400px)
+                    </p>
+                  </div>
+                </label>
+              )}
             </div>
             {fileName && (
               <p className="mt-2 text-sm text-green-500">{fileName}</p>
@@ -151,7 +172,7 @@ export default function CardDetailsModal({
                 UPLOAD NEW SELFIE
               </Button>
             </aside>
-          </div>{' '}
+          </div>
         </div>
       </div>
     </Modal>
